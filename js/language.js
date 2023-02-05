80d501041c92c6ea3b4894a3d13d15bef1dcb0ca
Pablo <pablosantoscerqueira@gmail.com>
2023-02-05 17:25:37 -0300

    Fix (Search Console) #2

--- js/language.js
+++ js/language.js
@@ -1,9 +1,9 @@
-// const en = document.querySelectorAll('.en');
-// const ptBR = document.querySelectorAll('.pt-br');
+const en = document.querySelectorAll('.en');
+const ptBR = document.querySelectorAll('.pt-br');
 
-// let href = window.location.href;
-// let lastElement = href.split('/')[href.split('/').length-1];
-// let link;
+let href = window.location.href;
+let lastElement = href.split('/')[href.split('/').length-1];
+let link;
 
 // if(localStorage.getItem('language') == null){
 //     localStorage.setItem('language', 'en');
@@ -12,31 +12,31 @@
 //     update();
 // }
 
-// en.forEach((element) => {
-//     element.onclick = () => {
-//         localStorage.setItem('language', 'en');
-//         update();
-//     }
-// });
+en.forEach((element) => {
+    element.onclick = () => {
+        localStorage.setItem('language', 'en');
+        update();
+    }
+});
 
-// ptBR.forEach((element) => {
-//     element.onclick = () => {
-//         localStorage.setItem('language', 'pt-br');
-//         update();
-//     }
-// });
+ptBR.forEach((element) => {
+    element.onclick = () => {
+        localStorage.setItem('language', 'pt-br');
+        update();
+    }
+});
 
-// function update() {
-//     if(localStorage.getItem('language') == 'en' && href.includes('pt-br')){
-//         link = href.replace('pt-br/'+lastElement, '');
-//         window.location.href = link;
-//     }else if(localStorage.getItem('language') == 'pt-br' && !href.includes('pt-br')){
-//         if(lastElement == 'index.html'){
-//             link = href.replace('index.html', 'pt-br/');
-//         }else{
-//             link = href + 'pt-br/';
-//         }
+function update() {
+    if(localStorage.getItem('language') == 'en' && href.includes('pt-br')){
+        link = href.replace('pt-br/'+lastElement, '');
+        window.location.href = link;
+    }else if(localStorage.getItem('language') == 'pt-br' && !href.includes('pt-br')){
+        if(lastElement == 'index.html'){
+            link = href.replace('index.html', 'pt-br/');
+        }else{
+            link = href + 'pt-br/';
+        }
 
-//         window.location.href = link;
-//     }
-// }
+        window.location.href = link;
+    }
+}
